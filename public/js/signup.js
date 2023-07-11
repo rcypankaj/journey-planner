a21e5c36dfaab209c6a3837abe57db91d6c7d89a
Pankaj <rcypankaj@gmail.com>
2023-07-11 18:34:19 +0530

    signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,23 @@
 /* eslint-disable */
 
-import axios from "axios";
-import { showAlert } from "./alert";
+import axios from 'axios';
+import { showAlert } from './alert';
 
 export const signup = async (name, email, password, passwordConfirm) => {
   try {
     const res = await axios({
-      method: "POST",
-      url: "/api/v1/users/signup",
-      data: { name, email, password, passwordConfirm },
+      method: 'POST',
+      url: '/api/v1/users/signup',
+      data: { name, email, password, passwordConfirm }
     });
-    if (res.data.status === "success") {
-      showAlert("success", "Sign up successfully");
+    if (res.data.status === 'success') {
+      showAlert('success', 'Sign up successfully');
       window.setTimeout(() => {
-        location.assign("/");
+        location.assign('/');
       }, 1500);
     }
     // console.log(res);
   } catch (err) {
-    showAlert("error", err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
